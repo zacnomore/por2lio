2c96648aacf807abc47ec911d82eecf76b72f9f8
Zac Svoboda <cczac1@gmail.com>
2022-06-06 19:12:52 -0400

    Placehold posts, home page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,22 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <main className={styles.root}>
+    <div className={styles.root}>
       <Head>
         <title>Zachary Svoboda</title>
         <meta name="description" content="Person" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <section>Hello</section>
-    </main>
+      <main className={styles.intro}>
+        <section>
+          <p>Hi</p>
+        </section>
+      </main>
+    </div>
   )
 }
 
---
+++ pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <section>
+        <p>Posts</p>
+      </section>
+    </main>
+  )
+}
+
+export default Home

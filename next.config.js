d215675848c06458967d0b4524ba66005f79582f
Zac Svoboda <cczac1@gmail.com>
2022-06-19 09:33:47 -0400

    Configure for mdx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
+const withMDX = require("@next/mdx")({
+  extension: /\.mdx?$/,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
 };
 
-module.exports = nextConfig;
+module.exports = withMDX(nextConfig);

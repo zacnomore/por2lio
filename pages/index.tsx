7df3efd202cff0ee36fec377cabf5e9f514766a3
Zachary Svoboda <cczac1@gmail.com>
2023-02-19 14:15:21 -0500

    Commit to the bit, deploy to prod

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,8 @@ const Home: NextPage = () => {
       <Layout title="Hi, my name is Zac.">
         <p>{`Here you can find my writing that I think is presently helpful or interesting.`}</p>
         <p>{`If you find something that isn't, please reach out so I can improve or remove it.`}</p>
+        <p>{`You will find the site is sparse at this particular moment. Every few years I like to refresh it
+        as an opportunity to play with new tools and toss out content that I feel no longer represents me.`}</p>
         <p></p>
       </Layout>
     </>

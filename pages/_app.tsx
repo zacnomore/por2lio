6e055afa2baa563181b833a63aaf8f33b6d3738d
Zachary Svoboda <cczac1@gmail.com>
2023-09-06 14:50:21 -0400

    Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Zachary Svoboda</title>
+        <meta
+          name="description"
+          content="Person, capable of some things and not others"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />;
       <Analytics />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,22 @@
 import type { NextPage } from "next";
-import Head from "next/head";
 import Layout from "../components/Layout";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Zachary Svoboda</title>
-        <meta
-          name="description"
-          content="Person, capable of some things and not others"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <Layout title="Zachary Svoboda" subTitle="Frontend Developer">
-        <p>
-          Welcome to my portfolio. My name is Zac, I&apos;ve been a developer
-          for {new Date().getFullYear() - 2016} years. I often work with Angular
-          and Typescript although I cultivate an interest in a number of web
-          technologies; this site is built with Next.js for example.
-        </p>
-        <p>
-          I enjoy contributing to open source, I&apos;m proud to have landed
-          some contributions to the Firefox browser devtools. When I&apos;m not
-          coding I like to garden and cook with my wife, nap with my cat and
-          play chess online.
-        </p>
-      </Layout>
-    </>
+    <Layout title="Zachary Svoboda" subTitle="Frontend Developer">
+      <p>
+        Welcome to my portfolio. My name is Zac, I&apos;ve been a developer for{" "}
+        {new Date().getFullYear() - 2016} years. I often work with Angular and
+        Typescript although I cultivate an interest in a number of web
+        technologies; this site is built with Next.js for example.
+      </p>
+      <p>
+        I enjoy contributing to open source, I&apos;m proud to have landed some
+        contributions to the Firefox browser devtools. When I&apos;m not coding
+        I like to garden and cook with my wife, nap with my cat and play chess
+        online.
+      </p>
+    </Layout>
   );
 };
 
